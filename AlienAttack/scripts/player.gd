96200394fcff7c2e0a07bc8aa447a88637609b85
Pat <59577400+patricks0510@users.noreply.github.com>
2024-05-09 17:45:03 -0400

    rockets shoot from spacebar now! asdf

--- AlienAttack/scripts/player.gd
+++ AlienAttack/scripts/player.gd
@@ -1,6 +1,11 @@
 extends CharacterBody2D
 
 var speed = 300
+var rocket_scene = preload("res://scenes/rocket.tscn")
+
+func _process(delta):
+	if Input.is_action_just_pressed("shoot_rocket"):
+		shoot()
 
 func _physics_process(delta):
 	velocity = Vector2(0,0)
@@ -14,20 +19,10 @@ func _physics_process(delta):
 		velocity.y = speed
 	move_and_slide()
 	
-	var screen_size = get_viewport_rect().size
-	#if global_position.x < 0:
-		#global_position.x = 0
-	#if global_position.x > screen_size.x:
-		#global_position.x = 1280
-	#if global_position.y < 0:
-		#global_position.y = 0
-	#if global_position.y > 720:
-		#global_position.y = 720
-		#
-	#global_position.x = clampf(global_position.x, 0, screen_size.x)
-	#global_position.y = clampf(global_position.y, 0, screen_size.y)
-	
+	var screen_size = get_viewport_rect().size	
 	global_position = global_position.clamp(Vector2(0,0), screen_size)
-	
-	
-	
+
+func shoot():
+	var rocket_instance = rocket_scene.instantiate()
+	add_child(rocket_instance)
+	rocket_instance.global_position.x += 80
