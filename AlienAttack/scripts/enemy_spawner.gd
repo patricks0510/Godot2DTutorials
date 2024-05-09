c501ce3566c2fc4010b12730e942e88463e26c2a
Pat <59577400+patricks0510@users.noreply.github.com>
2024-05-09 18:28:38 -0400

    random enemy spawning locations enabled

--- AlienAttack/scripts/enemy_spawner.gd
+++ AlienAttack/scripts/enemy_spawner.gd
@@ -1,5 +1,20 @@
 extends Node2D
 
+var enemy_scene = preload("res://scenes/enemy.tscn")
+
+#@onready var enemy_container = $EnemyContainer
+@onready var spawn_positions = $SpawnPositions
 
 func _on_timer_timeout():
-	pass # Replace with function body.
+	spawn_enemy()
+	
+func spawn_enemy():
+	var spawn_pos_array = spawn_positions.get_children()
+	var random_spawn_pos = spawn_pos_array.pick_random()
+	
+	var enemy_insntance = enemy_scene.instantiate()
+	enemy_insntance.global_position = random_spawn_pos.global_position
+	add_child(enemy_insntance)
+	
+	
+	
