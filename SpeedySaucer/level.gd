da83633e4de886ddb539e062943b5f0ec678d85c
Pat <59577400+patricks0510@users.noreply.github.com>
2024-05-09 16:09:15 -0400

    finished speedy saucer

--- SpeedySaucer/level.gd
+++ SpeedySaucer/level.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
 
-
-func _on_maze_body_exited(body):
+func _on_maze_body_exited(_body):
 	get_tree().reload_current_scene()
