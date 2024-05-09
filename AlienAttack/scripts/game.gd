22273f11d3acb2bf46ca0f95da6e6551b5f407c4
Pat <59577400+patricks0510@users.noreply.github.com>
2024-05-09 18:49:45 -0400

    created lives system for the player, custom signals created

--- AlienAttack/scripts/game.gd
+++ AlienAttack/scripts/game.gd
@@ -1,5 +1,15 @@
 extends Node2D
 
+var lives = 3
+
+@onready var player = $Player
 
 func _on_deathzone_area_entered(area):
 	area.die()
+
+func _on_player_took_damage():
+	lives -= 1
+	if lives == 0:
+		print("Game over!")
+		player.die()
+	print(lives)
