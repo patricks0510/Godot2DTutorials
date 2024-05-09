10736ba1a7b622dddabbc142571ffac853c63f31
Pat <59577400+patricks0510@users.noreply.github.com>
2024-05-09 17:51:46 -0400

    rockets no longer tied to player position

--- AlienAttack/scripts/player.gd
+++ AlienAttack/scripts/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 var speed = 300
 var rocket_scene = preload("res://scenes/rocket.tscn")
 
+@onready var rocket_container = $RocketContainer
+
 func _process(delta):
 	if Input.is_action_just_pressed("shoot_rocket"):
 		shoot()
@@ -24,5 +26,6 @@ func _physics_process(delta):
 
 func shoot():
 	var rocket_instance = rocket_scene.instantiate()
-	add_child(rocket_instance)
+	rocket_container.add_child(rocket_instance)
+	rocket_instance.global_position = global_position
 	rocket_instance.global_position.x += 80
