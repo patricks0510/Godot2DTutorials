14bd2960f5137019710fcf4cd593df1d04096ae1
Pat <59577400+patricks0510@users.noreply.github.com>
2024-05-14 16:31:55 -0400

    animated player and added jumping logic

---
+++ MartianMike/scripts/player.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+@export var gravity = 400
+@export var speed = 125
+@export var jump_force = 200
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta):
+	if !is_on_floor() && velocity.y < 500:
+		velocity.y += gravity * delta
+		
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		velocity.y = -jump_force
+		
+	var direction = Input.get_axis("move_left","move_right")
+	if(direction != 0):
+		animated_sprite.flip_h = (direction == -1)
+	velocity.x = direction * speed
+	move_and_slide()
+	
+	update_animations(direction)
+	
+func update_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		if velocity.y < 0:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("fall")
+
