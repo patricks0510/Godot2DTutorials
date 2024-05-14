50872cbefe0d3ab02633636eeb7132bc434b72a2
Pat <59577400+patricks0510@users.noreply.github.com>
2024-05-14 17:09:32 -0400

    created traps fro player to reset when touched

--- MartianMike/scripts/player.gd
+++ MartianMike/scripts/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+class_name Player
+
 @export var gravity = 400
 @export var speed = 125
 @export var jump_force = 200
@@ -11,7 +13,7 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 		
 	if Input.is_action_just_pressed("jump") && is_on_floor():
-		velocity.y = -jump_force
+		jump(jump_force)
 		
 	var direction = Input.get_axis("move_left","move_right")
 	if(direction != 0):
@@ -21,6 +23,9 @@ func _physics_process(delta):
 	
 	update_animations(direction)
 	
+func jump(force):
+	velocity.y = -force
+
 func update_animations(direction):
 	if is_on_floor():
 		if direction == 0:
