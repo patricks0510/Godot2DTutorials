d47e41352189060236724fd9b9ec6863e55381cb
Pat <59577400+patricks0510@users.noreply.github.com>
2024-05-15 21:17:47 -0400

    timer created in back end

--- MartianMike/scripts/level.gd
+++ MartianMike/scripts/level.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
 @export var nextLevel: PackedScene = null
+@export var level_time = 5
 
 @onready var start = $Start
 @onready var exit = $Exit
 @onready var deathzone = $Deathzone
 
 var player = null
+
+var timer_node = null
+var time_left
+var win = false
 
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
@@ -19,6 +24,15 @@ func _ready():
 	
 	exit.body_entered.connect(_on_exit_body_entered)
 	deathzone.body_entered.connect(_on_deathzone_body_entered)
+	
+	time_left = level_time
+	
+	timer_node = Timer.new()
+	timer_node.name = "Level Timer"
+	timer_node.wait_time = 1
+	timer_node.timeout.connect(_on_level_timer_timeout)
+	add_child(timer_node)
+	timer_node.start()
 	
 
 func _process(delta):
@@ -44,5 +58,13 @@ func _on_exit_body_entered(body):
 	if body is Player && nextLevel!=null:
 		exit.animate()
 		player.active = false
+		win = true
 		await get_tree().create_timer(1.5).timeout
 		get_tree().change_scene_to_packed(nextLevel)
+
+func _on_level_timer_timeout():
+	if win == false:
+		time_left -= 1	
+		if time_left < 0:
+			reset_player()
+			time_left = level_time
